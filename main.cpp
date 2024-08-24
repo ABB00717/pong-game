c4d7660339c56202f2a7ec0f8910961ff103d076
ABB00717 <eric94528@gmail.com>
2024-08-24 17:14:08 +0800

    Move the ball around

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
 #include <raylib.h>
+#include <vector>
+
+class Ball {
+public:
+  Ball(std::vector<double> position, std::vector<double> speed, double radius) {
+    this->position = position;
+    this->speed = speed;
+    this->radius = radius;
+  }
+
+  void Update() {
+    position[0] += speed[0];
+    position[1] += speed[1];
+  }
+
+  void Draw() {
+    DrawCircle(position[0], position[1], radius, WHITE);
+  }
+
+private:
+  std::vector<double> position;
+  std::vector<double> speed;
+  double radius;
+  
+};
 
 int main() {
   std::cout << "Hello, World!" << std::endl;
@@ -10,11 +35,16 @@ int main() {
   const int targetFPS = 144;
   InitWindow(screenWidth, screenHeight, title);
   SetTargetFPS(targetFPS);
+  
+  Ball ball({(double)screenWidth/2, (double)screenHeight/2}, {1, 1}, 20);
 
   while (WindowShouldClose() == false) {
     BeginDrawing();
+    ball.Update();
 
-    DrawCircle(screenWidth/2, screenHeight/2, 20, WHITE);
+    ClearBackground(BLACK);
+
+    ball.Draw();
     DrawRectangle(10, screenHeight/2-60, 25, 120, WHITE);
     DrawRectangle(screenWidth-35, screenHeight/2-60, 25, 120, WHITE);
     DrawLine(screenWidth/2, 0, screenWidth/2, screenHeight, WHITE);

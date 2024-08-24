e9f09ce9139fb9561a3ce90b6cee884725dff140
ABB00717 <eric94528@gmail.com>
2024-08-24 17:48:37 +0800

    Move the player's paddle

--- main.cpp
+++ main.cpp
@@ -14,11 +14,8 @@ public:
     position[0] += speed[0];
     position[1] += speed[1];
 
-    if (position[1]-radius <= 0 || position[1]+radius >= GetScreenHeight())
-      speed[1] *= -1;
-
-    if (position[0]-radius <= 0 || position[0]+radius >= GetScreenWidth())
-      speed[0] *= -1;
+    if (position[1]-radius <= 0 || position[1]+radius >= GetScreenHeight()) speed[1] *= -1;
+    if (position[0]-radius <= 0 || position[0]+radius >= GetScreenWidth()) speed[0] *= -1;
   }
 
   void Draw() {
@@ -32,6 +29,32 @@ private:
   
 };
 
+class Paddle {
+public:
+  Paddle(std::vector<double> position, double speed, double width, double height) {
+    this->position = position;
+    this->speed = speed;
+    this->width = width;
+    this->height = height;
+  }
+
+  void Draw() {
+    DrawRectangle(position[0], position[1], width, height, WHITE);
+  }
+
+  void Update() {
+    if (IsKeyDown(KEY_E) && position[1] >= 15) position[1] -= speed;
+    if (IsKeyDown(KEY_D) && position[1]+height <= GetScreenHeight()-15) position[1] += speed;
+  }
+
+private:
+  std::vector<double>  position;
+  double speed;
+  double width;
+  double height;
+
+};
+
 int main() {
   std::cout << "Hello, World!" << std::endl;
 
@@ -43,15 +66,17 @@ int main() {
   SetTargetFPS(targetFPS);
   
   Ball ball({(double)screenWidth/2, (double)screenHeight/2}, {3, 3}, 20);
+  Paddle player({10, (double)screenHeight/2-60}, 4, 25, 120);
 
   while (WindowShouldClose() == false) {
     BeginDrawing();
     ball.Update();
+    player.Update();
 
     ClearBackground(BLACK);
 
     ball.Draw();
-    DrawRectangle(10, screenHeight/2-60, 25, 120, WHITE);
+    player.Draw();
     DrawRectangle(screenWidth-35, screenHeight/2-60, 25, 120, WHITE);
     DrawLine(screenWidth/2, 0, screenWidth/2, screenHeight, WHITE);
     

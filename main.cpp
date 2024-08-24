258f50e6913bae669041d2e674db5839435e8b06
ABB00717 <eric94528@gmail.com>
2024-08-24 16:47:58 +0800

    Create a blank screen & Game loop

--- main.cpp
+++ main.cpp
@@ -7,7 +7,15 @@ int main() {
   const int screenWidth = 1280;
   const int screenHeight = 800;
   const char* title = "Pong!";
+  const int targetFPS = 144;
   InitWindow(screenWidth, screenHeight, title);
+  SetTargetFPS(targetFPS);
+
+  while (WindowShouldClose() == false) {
+    BeginDrawing();
+
+    EndDrawing();
+  }
 
   CloseWindow();
   return 0;
